32eb90ffc3bd51762c57979f41814e5f07fb7b5c
Seth <seth.pletcher@gmail.com>
2018-01-21 16:54:40 +0000

    Initial commit for spog spring boot application

---
+++ src/main/groovy/org/vsac/corp/spog/SpogApplication.groovy
@@ -0,0 +1,12 @@
+package org.vsac.corp.spog
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpogApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SpogApplication, args
+	}
+}
---
+++ src/test/groovy/org/vsac/corp/spog/SpogApplicationTests.groovy
@@ -0,0 +1,16 @@
+package org.vsac.corp.spog
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class SpogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
